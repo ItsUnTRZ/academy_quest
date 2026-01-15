dfd144c8939e559a83400d9810153868cec8dbdb
ธีรธร รักษาเมือง <unnie@MacBookAir.local>
2026-01-15 16:52:36 +0700

    chore: Add redirects for brag and root paths in routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :quests
   get "up" => "rails/health#show", as: :rails_health_check
   get "/brag" => "brag#index", as: :brag
+  get "/brag/*path", to: redirect("/brag")
+  get "*path", to: redirect("/")
   root "quests#index"
 end
