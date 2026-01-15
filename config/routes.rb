e9e66bab8278b951241463a302af9812809d0988
ธีรธร รักษาเมือง <unnie@MacBookAir.local>
2026-01-15 13:38:04 +0700

    feat: Add BragController, helper, view, and routes for brag feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :quests
   get "up" => "rails/health#show", as: :rails_health_check
+  get "/brag" => "brag#index", as: :brag
   root "quests#index"
 end
